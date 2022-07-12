8850f2e8f9bef1565511122c80829179a4f7ce76
craques <cmuzhanye@gmail.com>
2022-07-12 23:28:40 +0200

    updated theme for button

--- src/components/atoms/Button/Button.styles.ts
+++ src/components/atoms/Button/Button.styles.ts
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
 export const StyledButton = styled.button`
-  background-color: #3d4b6c;
-  padding: 8px;
-  font-size: 16px;
+  background-color: ${({ theme }) => theme.colors.bluePrimary};
+  padding: ${({ theme }) => theme.spacing.small}px;
+  font-size: ${({ theme }) => theme.spacing.large}px;
   color: white;
   font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
     monospace;
