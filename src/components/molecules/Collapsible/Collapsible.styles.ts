b0eaf7ce96f9e2129d1f3335c799fa6a8c94f994
craques <cmuzhanye@gmail.com>
2022-07-12 23:44:21 +0200

    Updated styles

--- src/components/molecules/Collapsible/Collapsible.styles.ts
+++ src/components/molecules/Collapsible/Collapsible.styles.ts
@@ -4,20 +4,21 @@ import { Info } from "@styled-icons/evaicons-solid/Info";
 import { CollapsibleProps } from "./Collapsible.types";
 
 export const StyledCaretDown = styled(CaretDown).attrs({ size: 24 })`
-  margin-right: 8px;
+  margin-right: ${({ theme }) => theme.spacing.small}px;
 `;
 
 export const StyledInfoIcon = styled(Info).attrs({ size: 24 })`
-  margin-left: 8px;
-  color: #a5b0c6;
+  margin-left: ${({ theme }) => theme.spacing.small}px;
+  color: ${({ theme }) => theme.colors.placeholderText};
 `;
 
 export const CollapsibleBody = styled.div<Partial<CollapsibleProps>>`
-  padding-left: 12px;
-  padding-right: 12px;
-  padding-top: ${({ isOpen }) => (isOpen ? 12 : 0)}px;
-  padding-bottom: ${({ isOpen }) => (isOpen ? 16 : 0)}px;
-  background-color: #28334d;
+  padding-left: ${({ theme }) => theme.spacing.medium}px;
+  padding-right: ${({ theme }) => theme.spacing.medium}px;
+  padding-top: ${({ isOpen, theme }) => (isOpen ? theme.spacing.medium : 0)}px;
+  padding-bottom: ${({ isOpen, theme }) =>
+    isOpen ? theme.spacing.large : 0}px;
+  background-color: ${({ theme }) => theme.colors.blueSecondary};
   height: ${({ isOpen }) => (isOpen ? "auto" : "0px")};
   visibility: ${(props) => (props.isOpen ? "visible" : "hidden")};
 `;
