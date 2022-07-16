1853df056f7a80636a2f37a44dac524ab73932e4
craques <cmuzhanye@gmail.com>
2022-07-16 21:04:23 +0200

    Added initial input tests

--- src/components/atoms/Input/Input.test.tsx
+++ src/components/atoms/Input/Input.test.tsx
@@ -1,14 +1,62 @@
-import { render, screen } from "@testing-library/react";
+import { act, fireEvent, render, screen } from "@testing-library/react";
 import { Input, InputProps } from ".";
 import { renderWithProviders } from "../../../helpers/testHelper";
+import { theme } from "../../../theme/theme";
+
+const onChangeMock = jest.fn();
+const onBlurMock = jest.fn();
+const onFocusMock = jest.fn();
 
 const getComponent = (props?: InputProps) => {
-  return <Input {...props} />;
+  return (
+    <Input
+      {...props}
+      onChange={onChangeMock}
+      onBlur={onBlurMock}
+      onFocus={onFocusMock}
+    />
+  );
 };
 
 describe("Input", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   it("should render without error", () => {
     renderWithProviders(getComponent());
     expect(screen.getByTestId("text-input")).toBeTruthy();
   });
+
+  it("should render the correct placeholder text", () => {
+    renderWithProviders(getComponent());
+    const input = screen.getByTestId("text-input");
+    expect(input).toHaveAttribute("placeholder", "auto");
+  });
+
+  it.skip("should fire on change event", () => {
+    renderWithProviders(getComponent());
+    const input = screen.getByTestId("text-input");
+    fireEvent.change(input, "h");
+    expect(onChangeMock).toHaveBeenCalled();
+  });
+
+  it("should render the correct value", () => {
+    renderWithProviders(getComponent({ value: "initialValue" }));
+    const input = screen.getByTestId("text-input");
+    expect(input).toHaveValue("initialValue");
+  });
+
+  it("should render the correct background color", async () => {
+    renderWithProviders(getComponent({ value: "initialValue" }));
+    const input = screen.getByTestId("text-input");
+
+    input.focus();
+    expect(input).toHaveFocus();
+    expect(input).toHaveStyle(
+      `background-color: ${theme.colors.blueSecondary}`,
+    );
+  });
+
+  it("should render outline on focus", () => {});
 });
