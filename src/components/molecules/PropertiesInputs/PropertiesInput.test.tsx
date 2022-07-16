7e011e3869864b7183bf52bec3ff47704c3d7524
craques <cmuzhanye@gmail.com>
2022-07-17 00:19:04 +0200

    Added tests for initial values

--- src/components/molecules/PropertiesInputs/PropertiesInput.test.tsx
+++ src/components/molecules/PropertiesInputs/PropertiesInput.test.tsx
@@ -1,3 +1,4 @@
+import { screen } from "@testing-library/react";
 import React from "react";
 import { PropertiesInput, PropertiesInputProps } from ".";
 import { renderWithProviders } from "../../../helpers/testHelper";
@@ -20,4 +21,61 @@ describe("PropertiesInput", () => {
       }),
     );
   });
+
+  it.each`
+    property     | testId
+    ${"margin"}  | ${"margin-left-input"}
+    ${"margin"}  | ${"margin-right-input"}
+    ${"margin"}  | ${"margin-top-input"}
+    ${"margin"}  | ${"margin-bottom-input"}
+    ${"padding"} | ${"padding-left-input"}
+    ${"padding"} | ${"padding-right-input"}
+    ${"padding"} | ${"padding-top-input"}
+    ${"padding"} | ${"padding-bottom-input"}
+    ${"border"}  | ${"border-left-input"}
+    ${"border"}  | ${"border-right-input"}
+    ${"border"}  | ${"border-top-input"}
+    ${"border"}  | ${"border-bottom-input"}
+  `(
+    "should render the correct properties inputs for $property",
+    ({ property, testId }) => {
+      renderWithProviders(
+        getComponent({
+          onBlur: onBlurMock,
+          onChange: onChangeMock,
+          property,
+          propertyValues: {},
+        }),
+      );
+
+      expect(screen.getByTestId(testId)).toBeTruthy();
+    },
+  );
+
+  it.each`
+    testId                   | value
+    ${"margin-top-input"}    | ${"10px"}
+    ${"margin-bottom-input"} | ${"15px"}
+    ${"margin-left-input"}   | ${"20px"}
+    ${"margin-right-input"}  | ${"30px"}
+  `(
+    "should render property values correctly if they are provided",
+    ({ testId, value }) => {
+      renderWithProviders(
+        getComponent({
+          onBlur: onBlurMock,
+          onChange: onChangeMock,
+          property: "margin",
+          propertyValues: {
+            "margin-left": "20px",
+            "margin-right": "30px",
+            "margin-top": "10px",
+            "margin-bottom": "15px",
+          },
+        }),
+      );
+
+      expect(screen.getByTestId(testId)).toHaveValue(value);
+    },
+  );
 });
